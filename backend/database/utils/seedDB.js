4eede2792513737fada98819ecb5385cd7746e70
Kevin Ye <kevinye4704@gmail.com>
2025-11-20 23:11:48 -0500

    feat: update seed data with complete field values

--- backend/database/utils/seedDB.js
+++ backend/database/utils/seedDB.js
@@ -11,30 +11,37 @@ const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
 		address: "695 Park Ave, New York, NY 10065",
-		description: "This is a school in New York, New York."
+		description: "This is a school in New York, New York.",
+		imageUrl: "https://s29068.pcdn.co/wp-content/uploads/hunter-campus-768x432.jpg.optimal.jpg"
 	});
 	// Create a new campus
 	const dummy_campus2 = await Campus.create({
 		name: "Queens College",
 		address: "65-30 Kissena Blvd, Queens, NY 11367",
-		description: "This is a school in Queens, New York."
+		description: "This is a school in Queens, New York.",
+		imageUrl: "https://upload.wikimedia.org/wikipedia/commons/d/df/Facade_of_The_Queen%27s_College%2C_Oxford%2C_2020.jpg"
 	});
 	// Create a new campus
 	const dummy_campus3 = await Campus.create({
 		name: "Brooklyn College",
 		address: "2900 Bedford Ave, Brooklyn, NY 11210",
-		description: "This is a school in Brooklyn, New York."
+		description: "This is a school in Brooklyn, New York.",
+		imageUrl: "https://www.cuny.edu/wp-content/uploads/sites/4/2015/01/09_20_2006_brc_quad_06.jpg"
 	});
 	
 	// Create a new student for a campus
 	const dummy_student = await Student.create({
 		firstname: "Joe",
-      lastname: "Smith",
+      	lastname: "Smith",
+		email: "joe.smith@myhunter.cuny.edu",
+    	gpa: 3.5
 	});
 	// Create a new student for a campus
 	const dummy_student2 = await Student.create({
 		firstname: "Mary",
-      lastname: "Johnson",
+      	lastname: "Johnson",
+		email: "mary.johnson@Qmail.cuny.edu",
+    	gpa: 3.8
 	});
 
 	// Add students to campuses
